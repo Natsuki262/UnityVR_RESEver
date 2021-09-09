95a98117b343eda5022dcea137173b4374581819
fgl0001 <kinatu.aren@gmail.com>
2021-09-09 18:01:47 +0900

    いったんコミット動画に沿ってやるので銃はシーンから削除キャラも作り直した

--- Assets/MouseLock.cs
+++ Assets/MouseLock.cs
@@ -17,15 +17,18 @@ public class MouseLock : MonoBehaviour
     //camera取得
     [SerializeField]
     GameObject playerCamera;
-
     Quaternion cameraRot, charactorRot;
+    //カメラの加速度
     [SerializeField]
-    float cameraACX,cameraACY;
+    float cameraACX, cameraACY;
+
+    //カーソル非表示表示状態
+    bool cusorOnof = true;
     void Start()
     {
         //ゲーム開始時の現在の角度を格納
         cameraRot = playerCamera.transform.localRotation;
-        charactorRot=transform.localRotation;
+        charactorRot = transform.localRotation;
 
 
 
@@ -34,13 +37,16 @@ public class MouseLock : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xRot = Input.GetAxisRaw("Mouse X");
-        float yRot = Input.GetAxisRaw("Mouse Y");
+        float xRot = Input.GetAxisRaw("Mouse X") * cameraACX;
+        float yRot = Input.GetAxisRaw("Mouse Y") * cameraACY;
         cameraRot *= Quaternion.Euler(-yRot, 0, 0);
         charactorRot *= Quaternion.Euler(0, xRot, 0);
 
         playerCamera.transform.localRotation = cameraRot;
         transform.localRotation = charactorRot;
+
+        UpdateCursolONOFF();
+
 
     }
     private void FixedUpdate()
@@ -49,11 +55,34 @@ public class MouseLock : MonoBehaviour
         charaMoveY = 0;
         charaMoveZ = 0;
 
-        charaMoveX = Input.GetAxis("Horizontal")*charaSpeed;
-        charaMoveZ = Input.GetAxisRaw("Vertical")*charaSpeed;
+        charaMoveX = Input.GetAxis("Horizontal") * charaSpeed;
+        charaMoveZ = Input.GetAxisRaw("Vertical") * charaSpeed;
 
         //transform.position += new Vector3(charaMoveX, 0, charaMoveZ);
         transform.position += playerCamera.transform.forward * charaMoveZ + playerCamera.transform.right * charaMoveX;
+
+    }
+    public void UpdateCursolONOFF()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            cusorOnof = false;
+
+
+        }
+        else if (Input.GetMouseButtonDown(0))
+        {
+            cusorOnof = true;
+        }
+
+        if (cusorOnof)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        else if (!cusorOnof)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
 
     }
 }
