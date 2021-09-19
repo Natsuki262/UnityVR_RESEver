8a8977c65eab177eace2d74f4ca09980d7d684ca
fgl0001 <kinatu.aren@gmail.com>
2021-09-20 04:04:37 +0900

    ひとまず弾が撃てるようにはなった

--- Assets/ShootGun.cs
+++ Assets/ShootGun.cs
@@ -13,17 +13,39 @@ public class ShootGun : MonoBehaviour
     [SerializeField]
     private float bulletPower = 300f;
 
+    [SerializeField]
+    private Transform muzzle;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        //RayÇÃèâä˙ê›íË
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        
+        transform.rotation = Quaternion.LookRotation(ray.direction);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 1f, LayerMask.GetMask("enemy")))
+        {
+            Destroy(this.gameObject);
+
+        }
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+
 
     }
+    void Shoot()
+    {
+        var bulletInstance = Instantiate<GameObject>(bullet, muzzle.position, muzzle.rotation);
+        bulletInstance.GetComponent<Rigidbody>().AddForce(bulletInstance.transform.forward * bulletPower);
+        Destroy(bulletInstance, 5f);
+    }
+
 }
